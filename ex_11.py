6d5686b640bbe4b6887201eaeaf4ef1d4eb6b92c
Diego Oliveira da Cruz <diego.cruz@al.infnet.edu.br>
2025-03-29 13:43:59 -0300

    alteração código

--- ex_11.py
+++ ex_11.py
@@ -1,20 +1,21 @@
 from scapy.all import *
 
-def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
-
-def get_mac_address(ip_address):
+def start_sniffing(interface):
+    sniff(iface=interface, store=False, prn=process_packet)
+    
+def get_mac(ip):
+    arp_request = ARP(pdst=ip)
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request = ARP(pdst=ip_address)
     arp_request_broadcast = broadcast / arp_request
-    answered_list = srp(arp_request_broadcast,timeout=1,verbose=False)
-    return answered_list[0][0][1].hwsrc
-def process_sniffed_packet(packet):
-    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
-        originalmac = get_mac_address(packet[scapy.ARP].psrc)
-        responsemac = packet[scapy.ARP].hwsrc
-    if originalmac != responsemac:
-        print("[*] ALERT!!! You are under attack, ARP table is being poisoned.!")
+    answered = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered[0][1].hwsrc
+
+def process_packet(packet):
+    if packet.haslayer(ARP) and packet[ARP].op == 2:  # ARP Response
+        real_mac = get_mac(packet[ARP].psrc)
+        response_mac = packet[ARP].hwsrc
+        if real_mac != response_mac:
+            print("[!] ARP Spoofing Attack Detected!")
 
 if __name__ == '__main__':
-    sniff("wlo1")
+    start_sniffing("enp0s3")
